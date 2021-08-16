535d228f3a2aafde053fbc2b22eafa7aecffc415
aneeshpotnistcs <aneeshpotnistcs@gmail.com>
2021-08-16 09:34:16 +0000

    replacing log4j with logback

--- src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
@@ -8,7 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
